173757a02518794ab7d7f7f5cee7c5a54c28cbab
Duncan <duncan.m.mcfarlane@gmail.com>
2015-11-11 11:29:34 -0500

    timeout

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, 
+         :validatable, :lockable, :timeoutable 
 
   has_many :beer_reviews
 
--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 20151031153336) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0,     null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.string   "first_name"
     t.string   "last_name"
     t.boolean  "admin",                  default: false
@@ -52,5 +55,6 @@ ActiveRecord::Schema.define(version: 20151031153336) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
 
 end
