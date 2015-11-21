1a3531ba62e76e56159f377100629d202c1fc8f3
Duncan <duncan.m.mcfarlane@gmail.com>
2015-11-21 04:21:59 -0500

    part of oauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031153336) do
+ActiveRecord::Schema.define(version: 20151116032738) do
 
   create_table "beer_reviews", force: :cascade do |t|
     t.integer  "rating"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20151031153336) do
     t.boolean  "admin",                  default: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
